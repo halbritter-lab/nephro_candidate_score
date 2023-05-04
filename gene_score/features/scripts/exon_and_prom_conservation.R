10f3fb7fe5933f456b3cf57507f936319bdc8088
ninarank <126774360+ninarank@users.noreply.github.com>
2023-05-04 12:05:33 +0200

    Create exon_and_prom_conservation.R

---
+++ gene_score/features/scripts/exon_and_prom_conservation.R
@@ -0,0 +1,117 @@
+# CAVE: unfinished => check TODO's !!
+
+# Title: Exon and promoter conservation scores and coding sequence lengths
+
+# load libraries
+library(tidyverse)
+library(biomaRt)
+library(progress)
+library(phastCons100way.UCSC.hg38)  # PhastCons score ranges from 0 to 1 and represents the probability that a given nucleotide is conserved
+
+# load canonical transcripts (from script "promoter_CpG_o2e_ratio.R")
+canon_trans <- read.csv(paste0("gene_score/features/results/canonical_TSS_", creation_date, ".csv"),
+                       na.strings = c("NA", "NaN", " ", ""))
+
+#TODO: check genes without canoncical transcripts !!!
+
+# download Ensembl data 
+bm_version <- 109  # Ensembl Biomart Genes version
+ensembl <- useEnsembl(biomart = "ensembl", 
+                      dataset = "hsapiens_gene_ensembl", 
+                      version = bm_version)
+
+exons_coordinates <- getBM(attributes = c("ensembl_gene_id",
+                                         "ensembl_transcript_id",
+                                         "chromosome_name",
+                                         "exon_chrom_start",
+                                         "exon_chrom_end",
+                                         "cds_start",
+                                         "cds_end",
+                                         "genomic_coding_start",
+                                         "genomic_coding_end"),
+                          filters = c("ensembl_transcript_id"),
+                          values = list(canon_trans$canon_transcript_id),
+                          mart = ensembl) %>% 
+  mutate(all_coding = case_when((cds_end - cds_start + 1) == (exon_chrom_end - exon_chrom_start + 1) ~ TRUE)) %>% 
+  na.omit()
+
+# TODO: CAVE!!! review this code with Bernt, especially the mutate statement. Why omit all NA? 
+# => leads to around 3000 genes getting excluded. Also, in Leitao's code, there is an additional TRUE ~ FALSE statement, but then you don't get any NA values for 'all_coding'
+
+
+## Exon conservation scores
+# function to return average of phastCons scores of a given transcript
+# loads phastCons scores per transcript and not per exon => faster
+avg_phastCons_score_per_transcript <- function(transcript_id){
+  trans_df <- exons_coordinates %>%
+    filter(ensembl_transcript_id == transcript_id)
+  scores <- gscores(phastCons100way.UCSC.hg38, 
+                    GRanges(seqnames = paste0("chr", trans_df$chromosome_name), 
+                             IRanges(start = trans_df$genomic_coding_start, end = trans_df$genomic_coding_end))) %>%
+    data.frame() %>% 
+    mutate(sum_phastCon_per_exon = default * width)  # calculate exon sum of phastCon scores back from exon average
+  
+  # calculate average phastCons score per transcript
+  avg <- sum(scores$sum_phastCon_per_exon)/sum(scores$width)
+             
+  return(avg)
+}
+
+# get average phasCons scores for each canonical transcript (long calculation time)
+pb <- progress_bar$new(total = length(unique(exons_coordinates$ensembl_gene_id)))
+
+avg_phasCons_scores <- exons_coordinates %>%  
+  dplyr::select(ensembl_gene_id, ensembl_transcript_id) %>% 
+  unique() %>% 
+  rowwise() %>% 
+  mutate(avg_phasCons = {
+    pb$tick()
+    avg_phastCons_score_per_transcript(ensembl_transcript_id)
+    })
+
+# write results
+write.csv(avg_phasCons_scores, 
+          paste0("gene_score/features/results/avg_phasCons_scores_per_transcript_" , creation_date, ".csv"), 
+          row.names=FALSE)
+
+
+
+## Promoter conservation scores
+# function to calculate avearge phastCons score of promoter
+avg_phastCons_score_promoter <- function(chrom, start, end){
+   scores <-  gscores(phastCons100way.UCSC.hg38, GRanges(seqnames=paste0("chr", chrom), IRanges(start=start:end, width=1))) %>%
+    data.frame() %>% 
+     .$default %>% 
+     mean(na.rm=TRUE)
+  return(scores)
+}
+
+# get average phasCons scores for each promoter (long calculation time)
+pb <- progress_bar$new(total = length(unique(canon_trans$ensembl_gene_id)))
+avg_phasCons_prom <- canon_trans %>% 
+  unique() %>% 
+  rowwise() %>% 
+  mutate(avg_phasCons_promoter = {
+    pb$tick()
+    avg_phastCons_score_promoter(chrom = chrom, start = start, end = end)
+  })
+
+# write results
+write.csv(avg_phasCons_prom, 
+          paste0("gene_score/features/results/avg_phasCons_promoter_" , creation_date, ".csv"), 
+          row.names=FALSE)
+
+
+## Coding sequence lengths
+CDS_length <- exons_coordinates %>%
+  mutate(CDS_length = genomic_coding_end - genomic_coding_start + 1) %>%
+  group_by(ensembl_transcript_id) %>%
+  summarize(CDS_length = sum(CDS_length)) %>% 
+  filter(ensembl_transcript_id %in% canon_trans$canon_transcript_id) %>% 
+  left_join(canon_trans[, c("ensembl_gene_id", "canon_transcript_id")], by = c("ensembl_transcript_id" = "canon_transcript_id")) %>% 
+  dplyr::select(ensembl_gene_id, CDS_length)
+
+# write results
+write.csv(CDS_length, 
+          paste0("gene_score/features/results/CDS_length_" , creation_date, ".csv"), 
+          row.names=FALSE)
