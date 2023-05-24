3ac9cdef84d220200dcdadc5bf61ee1bc368ebb2
ninarank <126774360+ninarank@users.noreply.github.com>
2023-05-24 10:43:26 +0200

    Update mgi_mpo.R

--- gene_score/features/scripts/mgi_mpo.R
+++ gene_score/features/scripts/mgi_mpo.R
@@ -1,8 +1,10 @@
+# Title: Genes associated with mouse phenotype ontology "MP:0005367" (= "renal/urinary system phenotype") and its children
+
 # load libraries
 library(tidyverse)	
 library(jsonlite) 
 
-# download mouse phenotype ontology
+# download mouse phenotype ontology (MPO)
 mgi_mpo_url <- "https://www.informatics.jax.org/downloads/reports/mp.json"
 download.file(mgi_mpo_url, 
               destfile = paste0("gene_score/features/raw/mgi_mpo_", creation_date, ".json"))
@@ -37,26 +39,30 @@ mpo_all_children_from_term <- function(term_input, relation_df) {
   return(unique(mpo_all_children))
 }
 
-# get all children of "MP:0005367" (= "renal/urinary system phenotype")
+# get all children of term "MP:0005367" (= "renal/urinary system phenotype")
 mpo_all_children <- c()
 kidney_mpo_terms <- mpo_all_children_from_term("MP_0005367", relations) %>% 
   str_replace("_", ":")
 
-######## load mouse/human orthology with phenotype annotations
+
+# download and load mouse/human orthology with phenotype annotations
 download_url <- "https://www.informatics.jax.org/downloads/reports/HMD_HumanPhenotype.rpt"
-# download.file(url = download_url, destfile = paste0("./HMD_HumanPhenotype_", creation_date, ".rpt"))
-hmd_hp <- read.table(paste0("./HMD_HumanPhenotype_", creation_date, ".rpt"), sep="\t", header=FALSE) %>% select(-V6)
-#hmd_hp <- read.table(paste0("./HMD_HumanPhenotype_2023-03-02.rpt"), sep="\t", header=FALSE) %>% select(-V6)
+download.file(url = download_url, 
+              destfile = paste0("gene_score/features/raw/HMD_HumanPhenotype_", creation_date, ".rpt"))
+hmd_hp <- read.table(paste0("gene_score/features/raw/HMD_HumanPhenotype_", creation_date, ".rpt"), sep="\t", header=FALSE) %>% select(-V6)
 colnames(hmd_hp) <- c("human_marker_symbol", "human_entrez_id", "mouse_marker_symbol", "mgi_marker_accession_id", "mpo_id")
 
-######## load genotypes and mammalian phenotype annotations
+
+# download and load genotypes and mammalian phenotype annotations
 download_url <- "https://www.informatics.jax.org/downloads/reports/MGI_PhenoGenoMP.rpt"
-#download.file(url = download_url, destfile = paste0("./MGI_PhenoGenoMP_", creation_date, ".rpt"))
-mgi_pg_mp <- read.delim(paste0("./MGI_PhenoGenoMP_", creation_date,  ".rpt"), header=FALSE)
-# mgi_pg_mp <- read.delim("./MGI_PhenoGenoMP_2023-03-02.rpt", header=FALSE)
+download.file(url = download_url, 
+              destfile = paste0("gene_score/features/raw/MGI_PhenoGenoMP_", creation_date, ".rpt"))
+
+mgi_pg_mp <- read.delim(paste0("gene_score/features/raw/MGI_PhenoGenoMP_", creation_date,  ".rpt"), header=FALSE)
 colnames(mgi_pg_mp) <- c("allellic_composition", "allele_symbols", "genetic_background", "mpo_id", "pubmed_id", "mgi_marker_accession_id")
 
-####### filter genotypes for association with "MP:0005367" "renal/urinary system phenotype" and its children
+
+# filter genotypes for association with "MP:0005367" (= "renal/urinary system phenotype") and its children
 mgi_pg_mp <- mgi_pg_mp %>% 
   filter(mpo_id %in% kidney_mpo_terms) 
 
@@ -66,14 +72,14 @@ mgi_pg_mp <- separate(data = mgi_pg_mp, col = mgi_marker_accession_id, into = c(
 # get human entrez IDs correlates that are associated with "MP:0005367" and its children in mice
 hmd_hp_mpo_kidney <- hmd_hp %>% 
   filter(mgi_marker_accession_id %in% unique(c(mgi_pg_mp$mgi_marker_accession_id1, mgi_pg_mp$mgi_marker_accession_id2))) %>% 
-  select(human_entrez_id) %>% 
+  dplyr::select(human_entrez_id) %>% 
   unique %>% 
   mutate(mpo_kidney_gene = TRUE) 
 
-  
-
 # write results
-write.csv(hmd_hp_mpo_kidney, paste0("./mgi_human_genes_associated_MP_0005367_" , creation_date, ".csv"), row.names=FALSE)
+write.csv(hmd_hp_mpo_kidney, 
+          paste0("gene_score/features/results/mgi_human_genes_associated_MP_0005367_" , creation_date, ".csv"), 
+          row.names=FALSE)
 
 
 ################################################################
@@ -81,10 +87,6 @@ write.csv(hmd_hp_mpo_kidney, paste0("./mgi_human_genes_associated_MP_0005367_" ,
 # Example: MGI:1339949 Adamts4
 hmd_hp_mpo_kidney %>% filter(mgi_marker_accession_id == "MGI:1339949") 
 # => does not include a kidney mpo term, only attributed to kidney phenotype in multigenic phenotype Adamts1<tm1Mapr>|Adamts4<tm1.1Boer>
-
-
-
-
 
 
 
