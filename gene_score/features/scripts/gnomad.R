7da5eac154f74b53d3e056f75c8255976d330c2e
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-12 17:03:13 +0200

    add more constraint metrics

--- gene_score/features/scripts/gnomad.R
+++ gene_score/features/scripts/gnomad.R
@@ -1,4 +1,4 @@
-# Title: gnomad loss of function values
+# Title: gnomad gene constraints
 
 # load libraries
 library(tidyverse)
@@ -13,9 +13,48 @@ download.file(url = download_url,
 gunzip(filename = "gene_score/features/raw/gnomad.v2.1.1.lof_metrics.by_gene.txt.bgz", 
        destname = "gene_score/features/raw/gnomad.v2.1.1.lof_metrics.by_gene.txt")
 
-# extract Z- and pLI-scores
-gnomad_lof <- read.delim("gnomad.v2.1.1.lof_metrics.by_gene.txt") %>%
-  dplyr::select(gene, gene_id, gene_type, syn_z, mis_z, pLI)
+# load and select gnomad gene constraints
+gnomad_constraints <- read.delim("gnomad.v2.1.1.lof_metrics.by_gene.txt") %>%
+  dplyr::select(gene, 
+                gene_id,
+                obs_mis,
+                exp_mis,
+                oe_mis,
+                mu_mis,
+                possible_mis,
+                obs_mis_pphen,
+                exp_mis_pphen,
+                oe_mis_pphen,
+                possible_mis_pphen,
+                obs_syn,
+                exp_syn,
+                oe_syn,
+                mu_syn,
+                possible_syn,
+                obs_lof,
+                mu_lof,
+                possible_lof,
+                exp_lof,
+                pLI,
+                pRec,
+                pNull,
+                oe_lof,
+                syn_z, 
+                mis_z, 
+                lof_z,
+                oe_lof_upper_rank,
+                n_sites,
+                classic_caf,
+                max_af,
+                p,
+                exp_hom_lof,
+                cds_length,
+                num_coding_exons,
+                gene_length
+                )
+
+# add prefix
+names(gnomad_constraints)[3:length(names(gnomad_constraints))] <- paste0("gnomad_", names(gnomad_constraints)[3:length(names(gnomad_constraints))])
 
 # write results
-write.csv(gnomad_lof, paste0("gene_score/features/results/gnomad_Z_pLI_", creation_date, ".csv"), row.names = FALSE)
+write.csv(gnomad_constraints, paste0("gene_score/features/results/gnomad_constraints_", creation_date, ".csv"), row.names = FALSE)
