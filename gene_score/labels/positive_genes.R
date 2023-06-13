dc26b09239c3559defd3ce7c5cb9861433747fc3
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-13 15:28:47 +0200

    Create positive_genes.R

---
+++ gene_score/labels/positive_genes.R
@@ -0,0 +1,9 @@
+# Title: Positive kidney genes
+
+# load libraries
+library(tidyverse)
+libary(utils)
+
+# load genes associated with kidney disease from kidney-genetics
+kg_version -
+kg_url <– paste0("https://raw.githubusercontent.com/halbritter-lab/kidney-genetics/main/analyses/merged/KidneyGenetics_MergeAnalysesSources.2023-05-18.csv
