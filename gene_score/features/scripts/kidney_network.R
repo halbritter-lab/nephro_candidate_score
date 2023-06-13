e11692fb147362bd99ec88103217fd24bfbb2e0c
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-13 09:13:02 +0200

    Create kidney_network.R

---
+++ gene_score/features/scripts/kidney_network.R
@@ -0,0 +1,11 @@
+# Title: Summed z-scores from Kidney Network (https://kidney.genenetwork.nl)
+
+# load libraries and source helper function
+library(tidyverse)
+library(R.utils)
+
+options(timeout=300) #download timeout = 5min
+
+source("../helper_functions.R") 
+
+
