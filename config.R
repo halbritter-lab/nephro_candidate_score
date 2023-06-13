4222e42aaeef6c7bfb08e4533938cc801f92116a
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-13 15:30:48 +0200

    Update config.R

--- config.R
+++ config.R
@@ -2,3 +2,5 @@
 
 PROJECT_DIR <- # set your project directory here
 creation_date <- strftime(as.POSIXlt(Sys.time(), "UTC", "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d")
+
+kidney-genetics_version <- "2023-05-18"  # date of kidney-genetics version, check on https://github.com/halbritter-lab/kidney-genetics/tree/main/analyses/merged for latest version  
