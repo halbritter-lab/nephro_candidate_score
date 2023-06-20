e6ec18b388f1a44c82d94fd185813caff666fca0
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-20 12:25:59 +0200

    add OMIM download link

--- config.R
+++ config.R
@@ -1,6 +1,8 @@
 # configuration file
 
-PROJECT_DIR <- # set your project directory here
+PROJECT_DIR <- "" # set your project directory here
 creation_date <- strftime(as.POSIXlt(Sys.time(), "UTC", "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d")
 
 kidney-genetics_version <- "2023-05-18"  # date of kidney-genetics version, check on https://github.com/halbritter-lab/kidney-genetics/tree/main/analyses/merged for latest version  
+
+omim_download_url <- "" # set your OMIM download link here
