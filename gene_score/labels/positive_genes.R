0efd62b8638d9871c31585aa379c62d8335d3b94
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-22 11:23:46 +0200

    Update positive_genes.R

--- gene_score/labels/positive_genes.R
+++ gene_score/labels/positive_genes.R
@@ -5,5 +5,5 @@ library(tidyverse)
 library(utils)
 
 # load genes associated with kidney disease from github repository kidney-genetics
-kg_url <– paste0("https://raw.githubusercontent.com/halbritter-lab/kidney-genetics/main/analyses/merged/KidneyGenetics_MergeAnalysesSources.", kidney-genetics_version, ".csv")
-kid_gen <– read.csv(kg_url)
+kg_url <- paste0("https://raw.githubusercontent.com/halbritter-lab/kidney-genetics/main/analyses/merged/KidneyGenetics_MergeAnalysesSources.", kidney-genetics_version, ".csv")
+kid_gen <- read.csv(kg_url)
