b2981598e8bbd1019dc635f675e7e19be43da855
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-22 11:48:52 +0200

    Update positive_genes.R

--- gene_score/labels/positive_genes.R
+++ gene_score/labels/positive_genes.R
@@ -5,8 +5,14 @@ library(tidyverse)
 library(utils)
 
 # load genes associated with kidney disease from github repository kidney-genetics
-kg_url <- paste0("https://raw.githubusercontent.com/halbritter-lab/kidney-genetics/main/analyses/merged/KidneyGenetics_MergeAnalysesSources.", kidney-genetics_version, ".csv")
+kg_version <- "2023-05-18"  # TODO: change 
+kg_url <- paste0("https://raw.githubusercontent.com/halbritter-lab/kidney-genetics/main/analyses/merged/KidneyGenetics_MergeAnalysesSources.", kg_version, ".csv")
 kid_gen <- read.csv(kg_url)
+
+# filter genes above a specific evidence count
+evid_thresh <- 0 # TODO change
+pos_genes <- kid_gen %>% 
+  filter(evidence_count >= evid_thresh)
 
 
 # TODO: selection of evidence group etc...
