e7ca831e77766247bba318ac2369ce5061c60ed0
ninarank <126774360+ninarank@users.noreply.github.com>
2023-06-22 15:05:27 +0200

    Update dispensible_genes.R

--- gene_score/labels/dispensible_genes.R
+++ gene_score/labels/dispensible_genes.R
@@ -1,5 +1,4 @@
 # UNFINISHED - CHECK TODOs!
-# General TODO: shorten
 # TODO: check ALPK1 in final df
 
 # Title: Dispensible genes - genes with homozygous loss of fucnction variants, but not associated with any phenotype in OMIM
@@ -16,8 +15,10 @@ download.file(url = download_url,
 unzip(paste0("gene_score/labels/raw/41586_2020_2308_MOESM4_ESM_", creation_date, ".zip"))
 
 # load homozygous knockout genes
-hom_ko_genes <- read.table(paste0("gene_score/labels/raw/supplement/supplementary_dataset_7_hom_ko_genes.txt")) %>% 
-  hgnc_id_from_symbol_grouped(tibble(value = hom_lof_genes$V1)) %>% 
+hom_ko_genes <- read.table(file.path(PROJECT_DIR, "gnomad", "supplement", "supplementary_dataset_7_hom_ko_genes.txt"))
+
+# get HGNC ID for from gene symbol  
+hom_ko_genes  <- hgnc_id_from_symbol_grouped(tibble(value = hom_ko_genes$V1)) %>% 
   tibble() %>% 
   dplyr::rename(c("hgnc_id" = ".")) %>% 
   drop_na(hgnc_id)
@@ -37,14 +38,22 @@ clean_names <- gsub(" ", "_", gsub("# ", "", names_col[1,]))
 omim_genes_hg38 <-  read.delim2(paste0("gene_score/labels/raw/genemap2", creation_date, ".txt"), 
                                 header = FALSE, 
                                 comment.char = "#") %>% 
-  dplyr::mutate_all(~ifelse(. == "", NA, .))
+  dplyr::mutate_all(~ ifelse(. == "", NA, .))
 
-colnames(omim_genes_hg38) <-  clean_names
+colnames(omim_genes_hg38) <- clean_names
 
 # filter for morbid genes (a gene associated with a phenotype in OMIM)
-omim_hg38_morbid <- omim_genes_hg38 %>% filter(!is.na(Phenotypes))
+omim_hg38_morbid <- omim_genes_hg38 %>% 
+  filter(!is.na(Phenotypes), !is.na(Approved_Gene_Symbol)) # so far no genes with Phenotypes but without Approved_Symbol exists in hom_ko_genes
 
-# get dispensible genes
-# TODO: filter out positive genes
+# get HGNC ID for omim morbid genes
+omim_hg38_morbid_hgnc <- hgnc_id_from_symbol_grouped(tibble(value = omim_hg38_morbid$Approved_Gene_Symbol)) %>%
+  tibble() %>% 
+  dplyr::rename(c("hgnc_id" = ".")) %>% 
+  drop_na(hgnc_id)
+
+# get dispensible genes (homozygous ko genes without Phenotypes entry in OMIM and not listed in kidney-genetics)
 disp_genes <- hom_ko_genes %>% 
-  filter(!(`Approved symbol` %in% omim_morbid_all$`Approved Symbol`), `Approved symbol` %in% hom_ko_genes_final) 
+  filter(!(hgnc_id %in% omim_hg38_morbid_hgnc$hgnc_id), !(hgnc_id %in% kid_gen$hgnc_id)) 
+
+
