d46a397b92a5e04483cf94475e8b23c265d67232
Nina Rank <nrank@NinasMacBookPro.local>
2023-10-12 15:13:48 +0200

    changed paths

--- gene_score/labels/scripts/positive_genes.R
+++ gene_score/labels/scripts/positive_genes.R
@@ -2,29 +2,35 @@
 
 # load libraries
 library(tidyverse)
-library(utils)
+library(R.utils)
+library(config)
+
+# read configs
+config_vars <- config::get(file = "config.yml")
+script_path <- "gene_score/labels"
+
+# save current working directory
+current_wd <- getwd()
+
+# set new working directory
+setwd(file.path(config_vars$PROJECT_DIR, script_path))
 
 # download and unzip genes associated with kidney disease from github repository kidney-genetics
-kg_url <- paste0("https://github.com/halbritter-lab/kidney-genetics/raw/main/analyses/A_MergeAnalysesSources/results/A_MergeAnalysesSources.", kidney_genetics_version, ".csv.gz")
+kg_url <- paste0("https://github.com/halbritter-lab/kidney-genetics/blob/main/analyses/A_MergeAnalysesSources/results/A_MergeAnalysesSources.", config_vars$kidney_genetics_version, ".csv.gz?raw=TRUE")
 
 download.file(url = kg_url,
-              destfile = paste0("gene_score/labels/raw/A_MergeAnalysesSources.", kidney_genetics_version, ".csv.gz"))
+              destfile = paste0("raw/A_MergeAnalysesSources.", config_vars$kidney_genetics_version, ".csv.gz"))
 
-gunzip(filename = paste0("gene_score/labels/raw/A_MergeAnalysesSources.", kidney_genetics_version, ".csv.gz"), 
-       destname = paste0("gene_score/labels/raw/A_MergeAnalysesSources.", kidney_genetics_version, ".csv"))
+gunzip(filename = paste0("raw/A_MergeAnalysesSources.", config_vars$kidney_genetics_version, ".csv.gz"), 
+       destname = paste0("raw/A_MergeAnalysesSources.", config_vars$kidney_genetics_version, ".csv"))
 
 # load data
-kid_gen <- read.csv(paste0("gene_score/labels/raw/A_MergeAnalysesSources.", kidney_genetics_version, ".csv")) %>% 
+kid_gen <- read.csv(paste0("raw/A_MergeAnalysesSources.", config_vars$kidney_genetics_version, ".csv")) %>% 
   rename(hgnc_id_int = "hgnc_id")
 
-# # filter genes above a specific evidence count
-# evid_thresh <- 0 # TODO change
-# pos_genes <- kid_gen %>% 
-#   filter(evidence_count >= evid_thresh)
-
-
-# TODO: selection of evidence group etc...
 
 # write results
+write.csv(kid_gen, paste0("results/positive_genes_", config_vars$creation_date, ".csv"), row.names = FALSE)
 
-write.csv(kid_gen, paste0("gene_score/labels/results/positive_genes_", creation_date, ".csv"), row.names = FALSE)
+# set back former working directory
+setwd(current_wd)
