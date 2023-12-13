8e8367dcd42c3378fb8c64ef74b66ce3c84b6625
Nina Rank <nrank@NinasMacBookPro.local>
2023-12-13 17:36:38 +0100

    added config, changed to gzipped file - incomplete

--- gene_score/gene_score_preprocessing_master.R
+++ gene_score/gene_score_preprocessing_master.R
@@ -7,17 +7,26 @@ library(R.utils)
 library(config)
 library(jsonlite)
 
+# define relative script path
+project_topic <- "nephrology"
+project_name <- "nephro_candidate_score"
+script_path <- "/gene_score/"
+
 # read configs
-config_vars <- config::get(file = "config.yml")
-script_path <- "gene_score"
+config_vars <- config::get(file = Sys.getenv("CONFIG_FILE"),
+                           config = project_topic)
 
 # set working directory
-setwd(file.path(config_vars$PROJECT_DIR, script_path))
+setwd(paste0(config_vars$projectsdir, project_name, script_path))
+
+# store wd
+init_wd <- getwd()
 
 # source additional functions
 source("hgnc_functions.R")
 source("helper_functions.R")
 
+##### HGNC table ##### 
 # download HGNC gene table from github repository "kidney-genetics"
 gene_table_url <- paste0("https://github.com/halbritter-lab/kidney-genetics/blob/main/analyses/B_AnnotationHGNC/results/non_alt_loci_set_coordinates.", config_vars$hgnc_gt_version, ".csv.gz?raw=TRUE")
 download.file(url = gene_table_url, destfile = paste0("raw/HGNC_", config_vars$hgnc_gt_version, ".csv.gz"))
@@ -25,20 +34,11 @@ download.file(url = gene_table_url, destfile = paste0("raw/HGNC_", config_vars$h
 #        destname = paste0("raw/HGNC_", config_vars$hgnc_gt_version, ".csv"))
 
 # load HGNC gene table and filter for protein-coding genes # TODO: change paths
-HGNC_table <- read_gzipped_csv(prefix = "raw/HGNC_", 
-                               file_date = config_vars$hgnc_gt_version) %>% 
+HGNC_table <- read_csv(paste0("raw/HGNC_", config_vars$hgnc_gt_version, ".csv.gz"), show_col_types = FALSE) %>% 
   filter(locus_group == "protein-coding gene") %>% 
   dplyr::select(hgnc_id, entrez_id, ensembl_gene_id, symbol, alias_symbol, prev_symbol) %>% 
   rowwise() %>% 
   mutate(hgnc_id_int = as.integer(str_replace(hgnc_id, "HGNC:", "")))
-
-
-# 
-# HGNC_table2 <- read.csv(paste0("raw/junk.csv")) %>% 
-#   filter(locus_group == "protein-coding gene") %>% 
-#   dplyr::select(hgnc_id, entrez_id, ensembl_gene_id, symbol, alias_symbol, prev_symbol) %>% 
-#   rowwise() %>% 
-#   mutate(hgnc_id_int = as.integer(str_replace(hgnc_id, "HGNC:", "")))
 
 # extract all protein coding gene symbols
 all_prot_coding_gene_symbols <- unlist(strsplit(c(HGNC_table$symbol, HGNC_table$alias_symbol, HGNC_table$prev_symbol), "\\|")) %>% unique()
@@ -48,11 +48,12 @@ all_prot_coding_gene_symbols <- unlist(strsplit(c(HGNC_table$symbol, HGNC_table$
 # get positive genes
 cat("get positive genes...")
 source("labels/scripts/positive_genes.R")
+setwd(init_wd)
 
-# get dipensible genes #TODO: test with stable internet connection
+# get dispensible genes #TODO: test with stable internet connection
 cat("get dispensible genes...")
 source("labels/scripts/dispensable_genes.R")
-
+setwd(init_wd)
 
 ##### FEATURES ##### 
 # get cellXgene features and join with HGNC table
