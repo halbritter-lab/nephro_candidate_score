9883feb64e3c165743a2f767c68740eee9ae69a1
ninarank <rankn_c@med0119>
2024-01-09 16:31:14 +0100

    change variable name, remove comments

--- gene_score/training/get_permutation_importance.sh
+++ gene_score/training/get_permutation_importance.sh
@@ -19,13 +19,13 @@ echo "host: $(hostname)"
 echo "conda environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 
 
-# Parse command-line arguments
+# parse command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
         --ID)
-            MY_ID="$2"
+            training_ID="$2"
             shift # past argument
             shift # past value
             ;;
@@ -37,25 +37,21 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# Check if the required argument is provided
-if [ -z "$MY_ID" ]; then
-    echo "Please provide the --myID argument."
+# check if the required ID argument is provided
+if [ -z "$training_ID" ]; then
+    echo "Please provide the --ID argument."
     exit 1
 fi
-
-# # Your existing script logic goes here, and you can use $MY_ID as needed
-# echo "Executing script with MY_ID=$MY_ID"
-
 
 file_path="/data/gpfs-1/users/rankn_c/work/halbritter/nephro_candidate_score/gene_score/training/get_permutation_importance.py"
 
 # python "$file_path" 2>&1 | tee slurm_out/verbose_out_$SLURM_JOB_ID.txt
 
-python "$file_path" --ID "$MY_ID" 2>&1 | tee slurm_out/verbose_out_$SLURM_JOB_ID.txt
+python "$file_path" --ID "$training_ID" 2>&1 | tee slurm_out/verbose_out_$SLURM_JOB_ID.txt
 
 echo "end"
 
 # Example: sbatch command
-# sbatch your_sbatch_script.sh --myID $MY_ID
+# sbatch your_sbatch_script.sh --myID $training_ID
 
 
