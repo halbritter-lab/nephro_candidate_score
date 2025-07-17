3c71187236fcbd0052e902670fac9fe6c9665176
ninarank <rankn_c@med0119>
2025-07-17 12:37:21 +0200

    Refactor: remove hardcoded path

--- gene_score/training/scripts/get_permutation_importance.sh
+++ gene_score/training/scripts/get_permutation_importance.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 #
-# Identify project and set log path
 #SBATCH --job-name=permutation_importance
 #SBATCH --output=gene_score/training/slurm_out/slurm_%j.out
-
-#
-# Set a required running time for the job.
 #SBATCH --time=1-00
-#
-# Reserve resouces in partition
 #SBATCH --partition medium
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -18,41 +12,42 @@ echo "date: $(date)"
 echo "host: $(hostname)"
 echo "conda environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 
+# Ensure CONFIG_FILE is set
+if [ -z "$CONFIG_FILE" ]; then
+    echo "Error: CONFIG_FILE environment variable is not set."
+    exit 1
+fi
 
-# parse command-line arguments
+# Extract project path from config
+project_path=$(yq -r .nephrology.ML_projectsdir "$CONFIG_FILE")
+
+# Parse command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
-
     case $key in
         --ID)
             training_ID="$2"
-            shift # past argument
-            shift # past value
+            shift
+            shift
             ;;
-        *)  
-            # Unknown option
+        *)
             echo "Unknown option: $1"
             exit 1
             ;;
     esac
 done
 
-# check if the required ID argument is provided
+# Check if ID argument was provided
 if [ -z "$training_ID" ]; then
     echo "Please provide the --ID argument."
     exit 1
 fi
 
-file_path="/data/gpfs-1/users/rankn_c/work/halbritter/nephro_candidate_score/gene_score/training/scripts/get_permutation_importance.py"
+# Construct path to Python script dynamically
+file_path="${project_path}/nephro_candidate_score/gene_score/training/scripts/get_permutation_importance.py"
 
-# python "$file_path" 2>&1 | tee slurm_out/verbose_out_$SLURM_JOB_ID.txt
-
-python "$file_path" --ID "$training_ID" 2>&1 | tee gene_score/training/
-slurm_out/verbose_out_$SLURM_JOB_ID.txt
+# Run script with dynamic path and logging
+python "$file_path" --ID "$training_ID" 2>&1 | tee gene_score/training/slurm_out/verbose_out_$SLURM_JOB_ID.txt
 
 echo "end"
-
-# Example: sbatch command
-# sbatch your_sbatch_script.sh --myID $training_ID
-
 
