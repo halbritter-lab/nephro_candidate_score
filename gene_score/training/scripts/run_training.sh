9a3916a4e6a9ca69d43b406599554133467b01d9
ninarank <rankn_c@med0119>
2025-07-17 11:55:17 +0200

    Refactor: remove hardcoded path

--- gene_score/training/scripts/run_training.sh
+++ gene_score/training/scripts/run_training.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Identify project and set log path
 #SBATCH --job-name=ML_training
 #SBATCH --output=gene_score/training/slurm_out/slurm_%j.out
-
-# Set a required running time for the job.
 #SBATCH --time=7-00
-
-# Reserve resouces in partition
 #SBATCH --partition medium
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -17,24 +12,23 @@ echo "date: $(date)"
 echo "host: $(hostname)"
 echo "conda environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 
-# # Run the command and capture its output
-# project_path=$(yq -r .nephrology.ML_projectsdir "$CONFIG_FILE")
+# Check if CONFIG_FILE is set
+if [ -z "$CONFIG_FILE" ]; then
+    echo "Error: CONFIG_FILE environment variable is not set."
+    exit 1
+fi
 
-# # Now $file_path contains the result
-# # echo "The file path is: $project_path"
+# Extract project path from YAML config
+project_path=$(yq -r .nephrology.ML_projectsdir "$CONFIG_FILE")
 
-# # append project path
-# gs_training_path="${project_path}nephro_candidate_score/gene_score/training"
+# Construct script path
+file_path="${project_path}/nephro_candidate_score/gene_score/training/scripts/run_training.py"
 
-
-# # gs_training_path="$project_path/nephro_candidate_score/gene_score/training"
-
-# python "$gs_training_path/run_training.py" 2>&1 | tee $gs_training_path/slurm_out/verbose_out_$SLURM_JOB_ID.txt
-
-
-file_path="/data/gpfs-1/users/rankn_c/work/halbritter/nephro_candidate_score/gene_score/training/scripts/run_training.py"
-
+# Run the training script
 python "$file_path" 2>&1 | tee gene_score/training/slurm_out/verbose_out_$SLURM_JOB_ID.txt
 
-
 echo "end"
+
+
+
+
