49aa9b03788facf6b1770457ec2c659d273dd30b
ninarank <rankn_c@med0119>
2025-07-18 12:06:57 +0200

    Refactor: delete finally unused file, remove hardcoded paths

--- variant_score/features_labels/scripts/extract_variant_features_per_chrom.sh
+++ variant_score/features_labels/scripts/extract_variant_features_per_chrom.sh
@@ -18,6 +18,20 @@ echo "date: $(date)"
 echo "host: $(hostname)"
 echo "conda environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 
+
+# Check if CONFIG_FILE is set
+if [ -z "$CONFIG_FILE" ]; then
+    echo "Error: CONFIG_FILE environment variable is not set."
+    exit 1
+fi
+
+# Extract project path from YAML config
+project_path=$(yq -r .nephrology.ML_projectsdir "$CONFIG_FILE")
+
+# Construct script path
+file_path="${project_path}/nephro_candidate_score/variant_score/features_labels/scripts/extract_variant_features_per_chrom.py"
+
+
 # parse command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -43,8 +57,11 @@ if [ -z "$chrom" ]; then
 fi
 
 
+
+
+
 # run python script
-python features_labels/scripts/extract_variant_features_per_chrom.py --CHROM "$chrom" 2>&1 | tee features_labels/slurm_out/verbose_out_chr"$chrom"_$SLURM_JOB_ID.txt
+python "$file_path" --CHROM "$chrom" 2>&1 | tee "${project_path}/nephro_candidate_score/variant_score/features_labels/slurm_out/verbose_out_chr${chrom}_${SLURM_JOB_ID}.txt"
 
 
 echo "end"
